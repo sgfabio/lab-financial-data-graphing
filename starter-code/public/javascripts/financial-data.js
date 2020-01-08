bc540c7d77b10f56179ff901768f9f0f070cb600
Fabio SantAnna <sgfabio@yahoo.com>
2020-01-08 15:25:16 -0300

    Iteration 1: DONE |  Iteration 2: WIP

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -3,7 +3,8 @@ function getBitCoinPrices() {
     axios
     .get('http://api.coindesk.com/v1/bpi/historical/close.json')
     .then(responseFromAPI => {
-    console.log("Response from API is: ", responseFromAPI.data);
+    console.log("Response from API is: ", responseFromAPI.data.bpi);
+    printChart(responseFromAPI.data.bpi)
     })
     .catch(err => {
     console.log("Error is: ", err);
@@ -12,4 +13,41 @@ function getBitCoinPrices() {
 
 
 getBitCoinPrices()
-  
+
+funtion printChart(stockData) {
+    const days = Object.keys(stockData);              // Eixo X
+    const closingPrices  = Object.values(stockData);  // Eixo Y
+
+
+
+}
+
+
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: dailyData,
+        datasets: [{
+            label: 'Closing quotes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
